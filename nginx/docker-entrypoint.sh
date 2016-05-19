6ba19eb71c68968804cd0079064357d921a0ed3e
Alexandr Vronskiy <shandy.developer@gmail.com>
2016-05-19 13:01:20 +0300

    New nginx alpine

--- nginx/docker-entrypoint.sh
+++ nginx/docker-entrypoint.sh
@@ -12,6 +12,6 @@ fi
 sed -i "s/<PHPFPM_HOST>/${PHPFPM_PORT_9000_TCP_ADDR:-phpfpm}/g;s/<PHPFPM_PORT>/${PHPFPM_PORT_9000_TCP_PORT:-9000}/g" /etc/nginx/conf.d/default.conf;
 sed -i "s/<DOMAIN_PLACEHOLDER>/${DOMAIN:-boodmo.local}/g" /etc/nginx/conf.d/default.conf;
 sed -i "s/<APPLICATION_ENV>/${APPLICATION_ENV:-dev}/g" /etc/nginx/conf.d/default.conf;
-sleep 10
+sleep 5
 
 exec "$@"
